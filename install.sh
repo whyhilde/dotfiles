d4f4cdecfb8dc7f23c67b2a081001e307d447714
whyhilde <whyhilde@gmail.com>
2025-08-15 17:50:21 +0500

    add install

---
+++ install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+set -e
+
+
+packages=(
+  "firefox"
+  "obs-studio"
+  "neovim"
+  "kitty"
+  "git"
+  "zsh"
+  "zsh-autosuggestions"
+  "zsh-syntax-highlighting"
+
+  "bat"
+  "eza"
+  "fzf"
+  "tldr"
+  "thefuck"
+  "git-delta"
+  "zoxide"
+
+  "tmux"
+  "btop"
+  "cava"
+  "opendoas"
+)
+
+# zsh: sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# p10k: git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# font: https://github.com/romkatv/powerlevel10k/blob/master/font.md
